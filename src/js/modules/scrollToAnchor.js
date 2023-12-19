9cc10b361fe44b4b27600baca90f113a65b88486
Kirill <kirill.zayats.99@mail.ru>
2023-12-19 21:24:31 +0300

    feat: add desktop version

--- src/js/modules/scrollToAnchor.js
+++ src/js/modules/scrollToAnchor.js
@@ -1,22 +1,23 @@
-import header from '../components/header';
+// import header from '../components/header';
 import helpers from '../helpers';
 
 /**
 * Модуль "Плавный переход к якорю"
 */
 const init = () => {
-	helpers.$document.on('click.anchor', '.js-to-anchor', (e) => {
-		e.preventDefault();
-		e.stopPropagation();
+	helpers.$document.on('click.anchor', '.js-to-anchor', (event) => {
+		event.preventDefault();
+		event.stopPropagation();
 
-		const id = $(e.currentTarget).attr('href');
-		const speed = $(e.currentTarget).data('speed') || 500;
+		const id = $(event.currentTarget).attr('href');
+		const speed = $(event.currentTarget).data('speed') || 500;
 		const offset = helpers.$header.css('position') === 'fixed' || helpers.$header.css('position') === 'absolute' ? -helpers.$header.outerHeight(true) : 0;
+		helpers.scrollTo($(id), speed, offset);
 
-		header.closeMenu().then(() => {
-			$('.js-burger').removeClass('is-active');
-			helpers.scrollTo($(id), speed, offset);
-		});
+		// header.closeMenu().then(() => {
+		// 	$('.js-burger').removeClass('is-active');
+		// 	helpers.scrollTo($(id), speed, offset);
+		// });
 	});
 };
 
